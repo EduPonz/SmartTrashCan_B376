4e1f40b3a277858980c8978f9039896120b9d157
David Michalik <dmicha16@student.aau.dk>
2017-04-03 15:15:26 +0200

    Removed the "Hello" crap

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-//hello edu!!!
-//Hello David
-//Hello people
-//hello me!!
-//hallo mi frend
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
