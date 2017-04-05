fba292644223efa6a78f0b50284971a10e00bd95
Vincent Joly <vjoly15@student.aau.dk>
2017-04-05 13:18:59 +0200

    if it works yay!

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,6 +1,8 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
 
+//hello hope this works guys!!!
+
 LoginWindow::LoginWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::LoginWindow)
