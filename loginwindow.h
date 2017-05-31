9c9697db7d5013e385215ca2c23c7aa2d5253f96
EduPonz <e.ponzs@gmail.com>
2017-05-31 17:52:39 +0200

    LoginWindow documentation

--- loginwindow.h
+++ loginwindow.h
@@ -28,19 +28,19 @@ class LoginWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit LoginWindow(QWidget *parent = 0);
-    ~LoginWindow();
+    explicit LoginWindow(QWidget *parent = 0); /*!<Assigns the UI. Connects the username and password fields to the loginButton so when return is pressed the button emits signal clicked().*/
+    ~LoginWindow(); /*!<Deletes the UI.*/
 
 signals:
-    void correctUser(int id);
-    void signUpSignal();
+    void correctUser(int id); /*!<Signal emitted when the user and password match with an entry on the database. The signal carries the user ID.*/
+    void signUpSignal(); /*!<Signal emitted when the loginSingupButton is pressed.*/
 
 public slots:
-    void on_loginButton_clicked(); /*!<Hey edu whatsasd up */
-    void on_loginSingupButton_clicked();
+    void on_loginButton_clicked(); /*!<Slot triggered when the loginButton is clicked. Check whether the user and password match with an entry on the database. If they do it emits signal correctUser(int id). If they do not, sets outPutLabel text to NOT_PERSON_FOUND.*/
+    void on_loginSingupButton_clicked(); /*!<Slot triggerer when the SingupButton is clicked. Emits signal signUpSignal().*/
 
 private slots:
-    void on_loginRemoveButton_clicked();
+    void on_loginRemoveButton_clicked(); /*!<Slot triggered when the loginRemoveButton is clicked. Calls to the UserDataBaseManager.userDatabaseDeleteAll() method to delete the entire database.*/
 
 private:
     QString userNameEntry;
