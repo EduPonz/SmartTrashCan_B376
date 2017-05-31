3e745055c10fbffbc338dcaeb877df75758e4066
David Michalik <dmicha16@student.aau.dk>
2017-05-31 14:20:14 +0200

    HistoryDatabaseManager documentation.

--- historydatabasemanager.h
+++ historydatabasemanager.h
@@ -1,3 +1,9 @@
+/*! \class HistoryDatabaseManager class.
+    \brief This class handles the historyData table in the SQlite database.
+    It stores the order history iformation of each user and displays it into the
+    order history table in the application, under OptionsWindow.
+*/
+
 #ifndef HISTORYDATABASEMANAGER_H
 #define HISTORYDATABASEMANAGER_H
 
@@ -39,9 +45,9 @@ public:
 
     bool historyDatabaseInsert(int userID, bool selected_small, bool selected_medium, bool selected_big,
                                int small_qty, int medium_qty, int big_qty, QString comms, int pickup_time,
-                               int item_price, int time_price, int total_price, QString time, int invoice);   /*!<Insterts the   */
-    QSqlQuery rowNumberRetrieve(int userID);
-    QSqlQuery historyDatabaseRetrieve(int userID);
+                               int item_price, int time_price, int total_price, QString time, int invoice);   /*!<Populates the table with the purchase history information to the corresponding user ID */
+    QSqlQuery rowNumberRetrieve(int userID);  /*!<Retrieves the number of rows with the same user ID. */
+    QSqlQuery historyDatabaseRetrieve(int userID); /*!<Retrieves the information from the historyData table to be insterted into the order history table. */
 
 private:
     QString TABLE_NAME;
@@ -49,7 +55,7 @@ private:
     UserDataBaseManager userDatabaseManager;
 
    // void trashInfoDatabaseConnect();
-    void historyDatabaseInit(); /*!<Creates the historyData table in the database with the corresponidng colunmns.   */
+    void historyDatabaseInit(); /*!<Creates the historyData table in the database with the corresponding colunmns.   */
     bool isExist(); /*!<Check whether the databse is open and can be edited. Returns false if it is closed. */
     bool historyDatabaseDelete(int userID); /*!< Deletes the user's account and corresponding ID row from the historyData table *if the user chooses to terminate their account.*/
 };
